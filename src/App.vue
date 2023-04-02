989e705ac038ac25386830b2803091da638346b1
tlagoda <thibautlagoda@hotmail.fr>
2023-04-02 12:50:55 +0200

    add style to welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="chat">
-    <div v-if="!joined">
-      <form @submit.prevent="join">
-        <label>Enter your name: </label>
-        <input v-model="name" />
-        <button type="submit">Send</button>
-      </form>
+    <div v-if="!joined" class="welcome-div">
+      <h1>Welcome!</h1>
+      <div class="form-div">
+        <form @submit.prevent="join">
+          <label>Enter your name: </label>
+          <input v-model="name" />
+          <button type="submit">Join! 🚀</button>
+        </form>
+      </div>
+      <h2>[#{{ name }}]</h2>
     </div>
     <div v-else class="chat-container">
       <div class="messages-container">
         <div v-for="message in messages">
-          [{{ message.name }}]: {{ message.text }}
+          [#{{ message.name }}]: {{ message.text }}
         </div>
       </div>
       <div v-if="typingDisplay">{{ typingDisplay }}</div>
@@ -45,6 +49,14 @@ onBeforeMount(() => {
   socket.on("message", (message) => {
     messages.value.push(message);
   });
+
+  socket.on("typing", ({ name, isTyping }) => {
+    if (isTyping) {
+      typingDisplay.value = `${name} is typing...`;
+    } else {
+      typingDisplay.value = "";
+    }
+  });
 });
 
 const join = () => {
@@ -54,9 +66,13 @@ const join = () => {
 };
 
 const sendMessage = () => {
-  socket.emit("createMessage", { text: messageText.value }, () => {
-    messageText.value = "";
-  });
+  socket.emit(
+    "createMessage",
+    { name: name.value, text: messageText.value },
+    () => {
+      messageText.value = "";
+    }
+  );
 };
 
 let timeout;
@@ -69,4 +85,81 @@ const emitTyping = () => {
 };
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.welcome-div {
+  font-family: "Roboto Mono", monospace;
+  display: flex;
+  flex-direction: column;
+}
+
+.chat {
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(
+    to right top,
+    #051937,
+    #004d7a,
+    #008793,
+    #00bf72,
+    #a8eb12
+  );
+}
+
+.welcome-div h1 {
+  color: white;
+  margin-top: 2vh;
+  font-size: 4rem;
+  text-align: center;
+}
+
+.welcome-div .form-div {
+  width: 30vw;
+  margin: 5vh auto;
+}
+
+.welcome-div form {
+  height: 50vh;
+  border: 1px solid white;
+  border-radius: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  margin: 0 auto;
+}
+
+.welcome-div form label {
+  font-size: 2rem;
+  color: white;
+}
+
+.welcome-div form input {
+  margin: 2vh;
+  padding: 1vh 1vw;
+  font-size: 1.3rem;
+  border-radius: 1rem;
+  font-family: "Roboto Mono", monospace;
+}
+
+.welcome-div form button {
+  font-size: 1.3rem;
+  padding: 1vh 1vw;
+  border-radius: 1rem;
+}
+
+.welcome-div form button:hover {
+  cursor: pointer;
+}
+
+.welcome-div h2 {
+  text-align: center;
+  color: white;
+  font-size: 3rem;
+}
+</style>
